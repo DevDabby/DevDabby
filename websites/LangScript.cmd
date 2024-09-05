06cb76891e5a5e261d06f7f178457b0ad3c7818c
DevDabby <146466422+DevDabby@users.noreply.github.com>
2024-09-05 19:22:01 +0100

    Update LangScript.cmd

--- websites/LangScript.cmd
+++ websites/LangScript.cmd
@@ -5,3 +5,41 @@ title X#
 goto MainMenu
 
 :MainMenu
+
+set OpenBrowser = OpenBrowser();
+
+set KillConsole = KillConsole();
+
+set clearPage = clear;
+
+set OpenExplorer = start("location: NULL; solution: explorer;");
+
+set OpenDesktop = start("location: Desktop;");
+
+:: Console
+
+set /p RUN=/^>
+
+if %RUN% == %OpenBrowser% (
+  start Chrome
+)
+
+if %RUN% == %KillConsole% (
+  exit
+)
+
+if %RUN% == showFiles(); (
+  tree
+)
+
+if %RUN% == %clearPage% (
+  cls
+)
+
+if %RUN% == %OpenExplorer% (
+  explorer
+)
+
+if %RUN% == %OpenDesktop% (
+  start Desktop
+)
